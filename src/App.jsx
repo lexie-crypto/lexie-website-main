850779d69c981d22e67730b09b72f4a62e238e46
Marek <markmagliocco@gmail.com>
2025-04-21 01:34:55 -0500

    added analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import LandingPage from './pages/LandingPage';
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   return (
     <div className="App">
       <LandingPage />
+      <Analytics />
     </div>
   );
 }
