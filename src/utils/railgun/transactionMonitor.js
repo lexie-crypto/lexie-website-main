a86d90d7c5dae1456310279ed204ca100665addf
Marek <markmagliocco@gmail.com>
2025-08-12 09:20:21 -0500

    upd txn mon

--- src/utils/railgun/transactionMonitor.js
+++ src/utils/railgun/transactionMonitor.js
@@ -545,7 +545,8 @@ export const monitorTransactionInGraph = async ({
       if (transactionType === 'shield') {
         events = await queryCommitments(chainId, blockNumber, txHash);
       } else if (transactionType === 'unshield') {
-        events = await queryUnshields(chainId, blockNumber, txHash);
+        // Use nullifiers for unshield (spent notes) – these include the 'nullifier' field
+        events = await queryNullifiers(chainId, blockNumber, txHash);
       } else if (transactionType === 'transfer') {
         events = await queryNullifiers(chainId, blockNumber, txHash);
       }
