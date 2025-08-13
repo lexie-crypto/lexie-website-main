1e82b5e70056b9d769660c3f24913747d209f2f9
Marek <markmagliocco@gmail.com>
2025-08-13 17:51:50 -0500

    upd submit url

--- api/gas-relayer/[[...path]].js
+++
@@ -1,9 +0,0 @@
-import { config as sharedConfig, handleGasRelayer } from '../proxy.js';
-
-export const config = sharedConfig;
-
-export default async function handler(req, res) {
-  return handleGasRelayer(req, res);
-}
-
-
