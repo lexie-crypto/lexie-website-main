f894fdf4479d54f705e82b1fb83f7dfc571cdfc0
Marek <markmagliocco@gmail.com>
2025-08-13 02:51:32 -0500

    switch to proxy

--- api/gas-relayer/address.js
+++ api/gas-relayer/address.js
@@ -1,4 +1,4 @@
-import { relayerAddressHandler } from '../../../../lexie-gas-relayer/src/backend/relayer-proxy-handlers.js';
+import { relayerAddressHandler } from '../../../lexie-gas-relayer/src/backend/relayer-proxy-handlers.js';
 
 export const config = {
   api: { bodyParser: false }
