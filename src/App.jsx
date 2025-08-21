ec3b9ce362d8a70f785a6f3c392e40a0f552f823
Marek <markmagliocco@gmail.com>
2025-08-20 22:55:55 -0500

    changed to vault pg instead of wallet

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ function App() {
         <div className="App">
           <Routes>
             <Route path="/" element={<LandingPage />} />
-            <Route path="/wallet" element={<WalletPage />} />
+            <Route path="/vault" element={<WalletPage />} />
           </Routes>
           
           {/* Toast notifications */}
