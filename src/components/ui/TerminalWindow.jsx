c128a49e9d3e5822ecc604670819d87d6fed6d50
Marek <markmagliocco@gmail.com>
2025-09-03 22:43:46 -0500

    upd terminal lighter

--- src/components/ui/TerminalWindow.jsx
+++ src/components/ui/TerminalWindow.jsx
@@ -36,7 +36,7 @@ export default function TerminalWindow({
       <div className="relative bg-black">
         <div className="px-8 pt-4 pb-6">{children}</div>
         {/* Solid blue background */}
-        <div className="absolute inset-0 bg-blue-900/15 pointer-events-none"></div>
+        <div className="absolute inset-0 bg-blue-900/20 pointer-events-none"></div>
       </div>
 
       {(footerLeft || footerRight) && (
