8c055dc352c83ba65ef5b1ccf5d868acb1ba3b9d
lexie-crypto <mark@lexiecrypto.com>
2025-09-17 21:51:19 -0500

    upd modal unlck iss

--- src/components/InjectedProviderButtons.jsx
+++ src/components/InjectedProviderButtons.jsx
@@ -57,7 +57,7 @@ const InjectedProviderButtons = ({ disabled }) => {
         </div>
       ) : (
         // Grid layout when other providers are available
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-3 sm:gap-4 lg:gap-6">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-3 sm:gap-4 lg:gap-6 justify-items-center">
           {/* Always show WalletConnect first */}
           <button
             onClick={onWalletConnect}
