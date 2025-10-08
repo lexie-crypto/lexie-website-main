31a2f5efec5ca6b36c5120c1dd3bfad2cc600ec5
lexie-crypto <mark@lexiecrypto.com>
2025-10-07 23:32:51 -0500

    add lexie chat scrollbar

--- src/pages/ChatPage.tsx
+++ src/pages/ChatPage.tsx
@@ -3,8 +3,10 @@ import '../styles/globals.css';
 import { LexieChat } from '../components/chat/LexieChat';
 
 const ChatPage = () => {
+  // Scrollbar styling is applied via CSS classes on containers
+
   return (
-    <div className="min-h-screen bg-black text-white">
+    <div className="min-h-screen bg-black text-white scrollbar-terminal">
       <LexieChat />
     </div>
   );
