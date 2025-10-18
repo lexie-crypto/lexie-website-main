bc7946a1d296d2bf31fce6c87d0bd9f305747843
lexie-crypto <mark@lexiecrypto.com>
2025-10-18 15:45:55 -0500

    add chat on mobile

--- src/pages/ChatPage.tsx
+++ src/pages/ChatPage.tsx
@@ -28,7 +28,7 @@ const ChatPage = () => {
   return (
     <div className={`${isInIframe ? 'h-screen' : 'min-h-screen'} bg-black text-white ${isInIframe ? '' : 'scrollbar-terminal'}`}>
       {!isInIframe && isMobile && <Navbar onLexieChatOpen={() => {}} />}
-      <div className={`${!isInIframe && isMobile ? '-mt-6' : ''}`}>
+      <div className={`${!isInIframe && isMobile ? '-mt-9' : ''}`}>
         <LexieChat />
       </div>
     </div>
