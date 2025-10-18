2b1961d9dec6c8303b589c6adbe78757cf21e1e2
lexie-crypto <mark@lexiecrypto.com>
2025-10-17 23:32:40 -0500

    upd dbl vault on mob

--- src/pages/WalletPage.jsx
+++ src/pages/WalletPage.jsx
@@ -9,7 +9,7 @@ import VaultDesktop from '../components/vault/VaultDesktop.jsx';
 const WalletPage = () => {
   // VaultDesktop now handles mobile/desktop rendering internally
   return (
-    <div className="relative min-h-screen w-full bg-black text-white overflow-x-hiddenscrollbar-terminal md:pt-0 pt-16">
+    <div className="relative min-h-screen w-full bg-black text-white overflow-x-hiddenscrollbar-terminal md:pt-0 pt-14">
       {/* Logo in top left - redirects to main site */}
       <div className="absolute md:top-6 md:left-5 top-2 left-2 z-50 md:pl-6 pl-2">
         <a
